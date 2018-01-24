9b4573f94c7eede89272abed239f50e779acfb60
dudueasy <dudueasy@126.com>
2018-01-24 14:17:03 +0800

    FTP simulator added

---
+++ FTP simulator/ftp_client.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+# __author__ = 'apolo'
+# _date_ = '2018/1/23 上午8:19'
+
+import socket, shlex, json, os
+
+# 初始化工作
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+# 自定义一个 bytes 转换器
+def get_bytes(n):
+    return str(n).encode()
+
+
+# 创建socket对象
+client_sk = socket.socket()
+
+# 连接服务器, 参数为服务器的ip和端口.
+address = ('127.0.0.1', 8002)
+
+print('client 开始连接服务器')
+client_sk.connect(address)
+
+while True:
+
+    # tips : 命令格式为 post | file_path
+    command = input('请输入要执行的命令和文件 : ')
+    input_command, input_path = command.split("|")
+
+    # 分割输入为两部分: 命令和文件地址
+    input_command = input_command.strip()
+    input_path = input_path.strip()
+
+    # 获取文件绝对路径, 文件名, 文件大小
+    file_path = os.path.join(BASE_DIR, input_path)
+    file_name = os.path.basename(file_path)
+    file_size = os.stat(file_path).st_size
+
+    # 封包, 包中包含了文件名和文件大小的数据, 用 | 分割
+    file_info = '%s|%s|%s' % (input_command, file_name, file_size)
+
+    client_sk.sendall(file_info.encode())
+    print('---信息已发送-->\n')
+    client_sk.recv(1024)
+
+    try:
+        # 读取并上传对应的文件数据
+        # 这里使用  rb 模式读取的数据已经是 bytes类型了.
+        with open(file_path, 'rb') as file_obj:
+
+            # 为了减少内存的使用. 我们用while循环来读取数据
+            # 每次读取1024个字节. 刚好对应send()每次的发送量
+            # 初始化已发送的数量. 用于控制循环
+            has_sent = 0
+            while has_sent < file_size:
+                data = file_obj.read(1024)
+                client_sk.sendall(data)
+                # print(data)
+                # 这里如果要获取数据的大小,应该直接获取 bytes 对象的长度, 如果直接
+                has_sent += len(data)
+
+
+    except Exception as e:
+        print(e)
+
+    else:
+        print('上传成功')
+
---
+++ FTP simulator/ftp_server.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+__author__ = 'apolo'
+_date_ = '2018/1/23 上午8:18'
+
+import socket, subprocess, json, shlex, traceback, os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+media_folder = 'media'
+
+# 创建一个socket对象
+server_sk = socket.socket()
+# 创建ip地址和端口
+address = ('127.0.0.1', 8002)
+# 绑定ip地址和端口
+server_sk.bind(address)
+# 开始监听, 参数中设置允许排队的连接数, 超出的连接会被服务器拒绝
+server_sk.listen(3)
+
+
+# 自定义一个 bytes 转换器
+def get_bytes(n):
+    return str(n).encode()
+
+
+while True:
+    # 等待客户端请求, 接受到请求的时候会获得一个元组. 获取元组的第一个对象作为连接对象
+    print('server开始等待客户端连接请求\n')
+    conn, addr = server_sk.accept()
+    print('已经和客户端 {0} 建立连接'.format(addr))
+
+    # 开始持续通信
+    while True:
+        try:
+            print('开始接收文件信息')
+            # 获取数据并且转换为unicode格式
+            data = conn.recv(1024).decode()
+
+            # 如果没有数据那就直接出本次持续通信, 等待建立新的连接
+            if not data:
+                break
+            input_command, file_name, file_size = data.split('|')
+            file_size = int(file_size)
+            print('文件大小为 %i'%(file_size))
+
+            conn.send('ready to receive file'.encode())
+
+            # 定义一个媒体文件夹路径
+            upload_to = os.path.join(BASE_DIR, media_folder, file_name)
+            media_folder = os.path.join(BASE_DIR, media_folder)
+            if not os.path.isdir(media_folder):
+                os.mkdir(media_folder)
+
+            try:
+                # 使用一个 while 循环来持续接收数据并写入文件
+                with  open(upload_to, 'ab') as file_obj:
+                    received_data_size = 0
+                    while received_data_size < file_size:
+                        data = conn.recv(1024)
+                        file_obj.write(data)
+                        received_data_size += len(data)
+                        # print('已接收数据 %i字节'%(received_data_size))
+
+            except Exception as e:
+                print(e)
+
+            else:
+                print('文件接收完成')
+        except Exception as e:
+            print(e)
+            break
